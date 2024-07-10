f2dce24c8e1f51ae83da606e8389dc5f049c93f1
BONIFAZ282 <69992871+BONIFAZ282@users.noreply.github.com>
2024-07-10 01:18:29 -0500

    reporte

--- src/app/Screens/ganancias/ganancias.component.ts
+++ src/app/Screens/ganancias/ganancias.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ReporteService } from 'src/app/Service/reporte.service';
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -40,4 +42,20 @@ export class GananciasComponent implements OnInit {
       window.location.href = '/login';
     });
   }
+
+  exportToExcel() {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.reportes);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+    this.saveAsExcelFile(excelBuffer, 'reporte_ganancias');
+  }
+
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+  }
 }
+
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
